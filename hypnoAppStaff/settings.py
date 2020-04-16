c79cf5d3355ce4d87e8747f2d19c6a4de42cd284
Hastari Utama <hypnotherapyapplication@gmail.com>
2020-04-16 15:52:09 +0700

    second commit

--- hypnoAppStaff/settings.py
+++ hypnoAppStaff/settings.py
@@ -126,10 +126,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+'''
 STATICFILES_DIRS = [
     # '.','static',
     os.path.join(BASE_DIR, 'static')
-]
+]'''
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 # Settings for sending email
 DEFAULT_FROM_EMAIL = 'hypnotherapyapplication@gmail.com'
